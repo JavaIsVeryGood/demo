99cb6e8cd25b0cf42a940af0726c6335dc0c560f
yifei.yuan <1020154356@qq.com>
2019-08-29 03:19:59 +0100

    add the result page and PageController

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication()
 @ComponentScan("com.example.demo.controller")
+@ComponentScan("com.example.demo.service")
 public class DemoApplication {
 
 	public static void main(String[] args) {
